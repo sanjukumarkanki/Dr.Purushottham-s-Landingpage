b386ec54c3bd4beac4cba55780bb18955f3c8b49
sanjukumarkanki <sanjukanki56429@gmail.com>
2024-03-30 16:17:54 +0530

    changes

--- components/patentsReviewsSection.php
+++ components/patentsReviewsSection.php
@@ -22,7 +22,7 @@
                         echo "<img src='" . $row["carouselImageUrl"] . "' alt='Patient Image' />";
                         echo "<div>";
                         echo "<span>" . $row["CarouselText"] . "</span>"; // Assuming reviewText holds the surgery information
-                        echo "<button>";
+                        echo "<button onclick='openVideo(\"" . $row['CarouselVideoLink'] . "\")'>";
                         echo "<img src='assets/playIcon.png' alt='Play Icon' />";
                         echo "<span>Watch Full Video</span>";
                         echo "</button>";
@@ -64,35 +64,6 @@
 
                 }
             }
-        });
-
-        document.addEventListener('DOMContentLoaded', function() {
-            var modal = document.getElementById('videoModal');
-            var videoFrame = document.getElementById('videoFrame');
-
-            // Get all play buttons
-            var playBtns = document.querySelectorAll('.play-btn');
-            playBtns.forEach(function(btn) {
-                btn.addEventListener('click', function() {
-                    var videoUrl = this.getAttribute('data-video-url');
-                    videoFrame.src = videoUrl;
-                    modal.style.display = 'block';
-                });
-            });
-
-            // Close modal when the close button or outside modal area is clicked
-            var closeBtn = document.querySelector('.close');
-            closeBtn.addEventListener('click', function() {
-                videoFrame.src = ''; // Clear video source
-                modal.style.display = 'none';
-            });
-
-            window.addEventListener('click', function(event) {
-                if (event.target === modal) {
-                    videoFrame.src = ''; // Clear video source
-                    modal.style.display = 'none';
-                }
-            });
         });
     </script>
 </div>
